dc93c58e4603549434d441dc0a07b6015f35eeb5
Trym Vegard Haavardsholm <trym.haavardsholm@its.uio.no>
2022-03-10 19:42:04 +0100

    Updated lab for 2022

--- camera_pose_solution.h
+++ camera_pose_solution.h
@@ -3,12 +3,12 @@
 #include <string>
 
 /// \brief Main Lab program.
-class Lab5
+class CameraPoseSolution
 {
 public:
   /// \brief Constructs the lab.
   /// \param data_path Optional path to dataset.
-  explicit Lab5(const std::string& data_path = "../data/");
+  explicit CameraPoseSolution(const std::string& data_path = "../data/");
 
   /// \brief Runs the lab.
   void run();
